296321dd5296d378c6b81161bad10e031827296a
Tanny Chavez <tanchavez@lbl.gov>
2025-02-02 15:20:39 -0800

    Add default values for api_keys

--- src/parameters.py
+++ src/parameters.py
@@ -20,9 +20,11 @@ class IOParameters(BaseModel):
     data_type: DataType = Field(description="data type")
     root_uri: str = Field(description="root uri")
     data_uris: list[str] = Field(description="data uris")
-    data_tiled_api_key: Optional[str] = Field(description="tiled api key")
+    data_tiled_api_key: Optional[str] = Field(description="tiled api key", default=None)
     results_tiled_uri: str = Field(description="tiled uri to save data to")
-    results_tiled_api_key: Optional[str] = Field(description="tiled api key")
+    results_tiled_api_key: Optional[str] = Field(
+        description="tiled api key", default=None
+    )
     uid_save: str = Field(description="uid to save data to")
     results_dir: str = Field(description="results directory")
     load_model_path: Optional[str] = None
